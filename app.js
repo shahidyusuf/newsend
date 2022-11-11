90f6f73b529f4c39b8a634eb029078734363e284
shahidyusuf <117685623+shahidyusuf@users.noreply.github.com>
2022-11-11 20:55:42 +0500

    testinge
    send to u test

---
+++ app.js
@@ -0,0 +1,23 @@
+//function addtocart(){
+//    var img=document.getElementById("product");
+//    var name=document.getElementById("name");
+//    var price=document.getElementById("price");
+//var cartimg=document.getElementById("cart-product");
+//var cartname=document.getElementById("cart-name");
+//var cartprice=document.getElementById("cart-price");
+//
+////cartimg=img.src
+//cartname.innerHTML=name.innerHTML
+////cartprice.innerHTML=price.innerHTML
+//}    
+
+var target = document.getElementById("P1");
+//target.setAttribute('class', "blue")
+console.log(target.attributes[1].nodeValue)
+
+//if(target.hasAttribute("class")){
+//    alert("available")
+//}
+//else{
+//    alert("not available")
+//}
